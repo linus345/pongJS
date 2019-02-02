aba2f9da04297e8d9a550a2cc01dcdfc89f07e9b
linus345 <linusekman01@gmail.com>
2019-02-02 22:44:54 +0100

    Each player can now move up and down

--- main.js
+++ main.js
@@ -8,14 +8,70 @@ canvas.height = 600;
 
 // define player
 class Player {
-  constructor(x, y) {
+  constructor(x, y, color) {
     this.x = x;
     this.y = y;
+    this.color = color;
+  }
 
-    ctx.fillStyle = '#fff';
+  start() {
+    this.draw();
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, 10, 80);
+  }
+
+  moveDown() {
+    this.y += 10;
+  }
+
+  moveUp() {
+    this.y -= 10;
   }
 }
 
-const player1 = new Player(50, 300);
-const player2 = new Player(550, 300);
+// player 1
+const player1 = new Player(50, 300, '#f00');
+// player 2
+const player2 = new Player(550, 300, '#0f0');
+
+window.addEventListener('keydown', e => movePlayer(e));
+
+function movePlayer(e) {
+  const { key } = e;
+  // move player 1 or move player 2
+  if(key === 'w' || key === 's') {
+    switch(key) {
+      case 'w':
+        player1.moveUp();
+        break;
+      case 's':
+        player1.moveDown();
+        break;
+      default:
+        break;
+    }
+  } else if(key === 'ArrowUp' || key === 'ArrowDown') {
+    switch(key) {
+      case 'ArrowUp':
+        player2.moveUp();
+        break;
+      case 'ArrowDown':
+        player2.moveDown();
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+function play() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player1.start();
+  player2.start();
+  window.requestAnimationFrame(play);
+}
+
+window.requestAnimationFrame(play);
